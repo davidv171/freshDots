f7224c1200395f2d5a070abb65c847a556825da1
davidv171 <david.vuckovic7@gmail.com>
2019-02-26 19:17:08 +0100

    paper theme

---
+++ bspwm/out.sh
@@ -0,0 +1,15 @@
+bspc wm -l /home/davidv7/.config/bspwm/out_canvas
+
+kitty  & disown
+kitty  & disown
+kitty  & disown
+kitty  & disown
+kitty  & disown
+kitty  & disown
+kitty --class=scratchpad sh /home/davidv7/.config/bspwm/scratch  & disown
+/usr/lib/firefox/firefox -p default  & disown
+MellowPlayer  & disown
+/opt/discord/Discord  & disown
+kitty --class=scratchpad2  & disown
+/opt/tc-twitch/tc  & disown
+kitty --class=scratchpad3  & disown
